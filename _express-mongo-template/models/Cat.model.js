5119cb47b66547988e9ab722da56d21d5890ef8b
alikahwaji <ali.kahwaji@hotmail.com>
2021-09-21 03:07:38 +1200

    Completed queries and model

---
+++ _express-mongo-template/models/Cat.model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { Schema, model } = mongoose;
+
+const catSchema = new Schema({
+    name: { type: String, required: true },
+    breed: { type: String },
+    age: { type: Number },
+    color: { type: String },
+    hasLegs: { type: Number },
+}, { timestamps: true });
+
+exports.Cat = model("Cats", catSchema);
